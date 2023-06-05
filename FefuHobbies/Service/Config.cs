d212d3ae035fbe98c39178bc83ff9ac4c8f27908
MrButterDuck <52395896+MrButterDuck@users.noreply.github.com>
2023-06-05 14:56:19 +1000

    Delete FefuHobbies directory

--- FefuHobbies/Service/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FefuHobbies.Service
-{
-	public class Config
-	{
-		public static string ConnectionString { get; set; }
-        public static string Email { get; set; }
-        public static string Name { get; set; }
-    }
-}
